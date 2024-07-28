75f98e3645d20060755b9ae60c9e3668e0e010cb
TsXor <zhang050525@outlook.com>
2024-07-28 20:25:19 +0800

    peinfo: support amd64 files

--- examples/peinfo.cpp
+++ examples/peinfo.cpp
@@ -73,42 +73,67 @@ int main(int argc, char *argv[]) {
 
     auto& doshdr = *reinterpret_cast<pe::image::dos_header*>(file_buf.data());
     if (doshdr.e_magic != pe::image::dos_signature) {
-        std::cout << "不是PE文件" << std::endl;
+        std::cout << "不是PE文件：DOS签名错误" << std::endl;
         return 0;
     }
 
     auto& nthdr = doshdr.nthdr();
     if (nthdr.Signature != pe::image::nt_signature) {
-        std::cout << "不是PE文件" << std::endl;
-        return 0;
-    }
-    if (nthdr.machine() != pe::image::file_machine::i386) {
-        std::cout << "不是x32文件" << std::endl;
+        std::cout << "不是PE文件：NT签名错误" << std::endl;
         return 0;
     }
 
     std::cout << std::left;
 
     auto& coffhdr = nthdr.FileHeader;
-    auto& opthdr = nthdr.OptionalHeader.x32;
-    std::cout
-        << "================== 基 本 P E 头 信 息 ==================" << std::endl
-        << "入口点:         " << pad_hex(opthdr.AddressOfEntryPoint)    << std::endl
-        << "子系统:         " << pad_hex(opthdr.Subsystem)              << std::endl
-        << "镜像基址:       " << pad_hex(opthdr.ImageBase)              << std::endl
-        << "区段数目:       " << pad_hex(coffhdr.NumberOfSections)      << std::endl
-        << "镜像大小:       " << pad_hex(opthdr.SizeOfImage)            << std::endl
-        << "日期时间标志:   " << pad_hex(coffhdr.TimeDateStamp)         << std::endl
-        << "代码基址:       " << pad_hex(opthdr.BaseOfCode)             << std::endl
-        << "文件头大小:     " << pad_hex(opthdr.SizeOfHeaders)          << std::endl
-        << "数据基址:       " << pad_hex(opthdr.BaseOfData)             << std::endl
-        << "特征值:         " << pad_hex(coffhdr.Characteristics)       << std::endl
-        << "块对齐:         " << pad_hex(opthdr.SectionAlignment)       << std::endl
-        << "校验和:         " << pad_hex(opthdr.CheckSum)               << std::endl
-        << "文件块对齐:     " << pad_hex(opthdr.FileAlignment)          << std::endl
-        << "可选头部大小:   " << pad_hex(coffhdr.SizeOfOptionalHeader)  << std::endl
-        << "标志字:         " << pad_hex(opthdr.Magic)                  << std::endl
-        << "RVA数及大小:    " << pad_hex(opthdr.NumberOfRvaAndSizes)    << std::endl;
+    pe::image::data_directory* datadirs;
+
+    if (nthdr.machine() == pe::image::file_machine::i386) {
+        auto& opthdr = nthdr.OptionalHeader.x32;
+        std::cout
+            << "================== 基 本 P E 头 信 息 ==================" << std::endl
+            << "入口点:         " << pad_hex(opthdr.AddressOfEntryPoint)    << std::endl
+            << "子系统:         " << pad_hex(opthdr.Subsystem)              << std::endl
+            << "镜像基址:       " << pad_hex(opthdr.ImageBase)              << std::endl
+            << "区段数目:       " << pad_hex(coffhdr.NumberOfSections)      << std::endl
+            << "镜像大小:       " << pad_hex(opthdr.SizeOfImage)            << std::endl
+            << "日期时间标志:   " << pad_hex(coffhdr.TimeDateStamp)         << std::endl
+            << "代码基址:       " << pad_hex(opthdr.BaseOfCode)             << std::endl
+            << "文件头大小:     " << pad_hex(opthdr.SizeOfHeaders)          << std::endl
+            << "数据基址:       " << pad_hex(opthdr.BaseOfData)             << std::endl
+            << "特征值:         " << pad_hex(coffhdr.Characteristics)       << std::endl
+            << "块对齐:         " << pad_hex(opthdr.SectionAlignment)       << std::endl
+            << "校验和:         " << pad_hex(opthdr.CheckSum)               << std::endl
+            << "文件块对齐:     " << pad_hex(opthdr.FileAlignment)          << std::endl
+            << "可选头部大小:   " << pad_hex(coffhdr.SizeOfOptionalHeader)  << std::endl
+            << "标志字:         " << pad_hex(opthdr.Magic)                  << std::endl
+            << "RVA数及大小:    " << pad_hex(opthdr.NumberOfRvaAndSizes)    << std::endl;
+        datadirs = opthdr.DataDirectory;
+    } else if (nthdr.machine() == pe::image::file_machine::amd64) {
+        auto& opthdr = nthdr.OptionalHeader.x64;
+        std::cout
+            << "================== 基 本 P E 头 信 息 ==================" << std::endl
+            << "入口点:         " << pad_hex(opthdr.AddressOfEntryPoint)    << std::endl
+            << "子系统:         " << pad_hex(opthdr.Subsystem)              << std::endl
+            << "镜像基址:       " << pad_hex(opthdr.ImageBase)              << std::endl
+            << "区段数目:       " << pad_hex(coffhdr.NumberOfSections)      << std::endl
+            << "镜像大小:       " << pad_hex(opthdr.SizeOfImage)            << std::endl
+            << "日期时间标志:   " << pad_hex(coffhdr.TimeDateStamp)         << std::endl
+            << "代码基址:       " << pad_hex(opthdr.BaseOfCode)             << std::endl
+            << "文件头大小:     " << pad_hex(opthdr.SizeOfHeaders)          << std::endl
+            << "数据基址:       " << "N/A in x64"                           << std::endl
+            << "特征值:         " << pad_hex(coffhdr.Characteristics)       << std::endl
+            << "块对齐:         " << pad_hex(opthdr.SectionAlignment)       << std::endl
+            << "校验和:         " << pad_hex(opthdr.CheckSum)               << std::endl
+            << "文件块对齐:     " << pad_hex(opthdr.FileAlignment)          << std::endl
+            << "可选头部大小:   " << pad_hex(coffhdr.SizeOfOptionalHeader)  << std::endl
+            << "标志字:         " << pad_hex(opthdr.Magic)                  << std::endl
+            << "RVA数及大小:    " << pad_hex(opthdr.NumberOfRvaAndSizes)    << std::endl;
+        datadirs = opthdr.DataDirectory;
+    } else {
+        std::cout << "暂不支持此架构的PE文件" << std::endl;
+        return 0;
+    }
 
     std::cout
         << "======================= 目 录 表 =======================" << std::endl
@@ -119,8 +144,8 @@ int main(int argc, char *argv[]) {
     for (size_t idx = 0; idx < 15; ++idx) {
         std::cout
             << std::setw(15) << directory_name(idx)
-            << std::setw(15) << pad_hex(opthdr.DataDirectory[idx].VirtualAddress)
-            << std::setw(15) << pad_hex(opthdr.DataDirectory[idx].Size)
+            << std::setw(15) << pad_hex(datadirs[idx].VirtualAddress)
+            << std::setw(15) << pad_hex(datadirs[idx].Size)
         << std::endl;
     }
 
